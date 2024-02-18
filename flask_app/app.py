2d33f3705c0882adfbe598e5ea466ffbf974c88f
Eric Wang <eriwang@nuevaschool.org>
2024-02-18 07:00:06 -0800

    Co-authored-by: jasonychen907@gmail.com <jasonychen907@gmail.com> Co-authored-by: akuroanemo <akuroanemo@users.noreply.github.com>

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, g, session
-from flask_session import Session
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 import numpy as np
@@ -9,8 +8,6 @@ from src.llm_summary import cluster_summaries
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///questions.db'
-    SESSION_TYPE = 'redis'
-    Session(app)
 
     db = SQLAlchemy(app)
 
@@ -72,11 +69,14 @@ def create_app():
                 df['sent'] = questions
                 df = cluster(df, 1.2)
                 cluster_dict = cluster_df_to_dict(df)
+                app.cluster_dict = cluster_dict
                 summaries = cluster_summaries(cluster_dict)
 
                 summary_text = []
                 for i,sum in enumerate(summaries):
-                    summary_text.append(f"{i+1}. {sum}")
+                    summary_text.append(f"{i+1}. {sum} (Num Questions: {len(cluster_dict[i])})")
+                
+                print(cluster_dict)
 
                 app.summary_text = summary_text
 
@@ -90,11 +90,20 @@ def create_app():
     def student():
         return render_template('student.html', session_status=app.session_status)
     
+    @app.route('/question_clusters')
+    def question_clusters():
+        return render_template('question_clusters.html', cluster_dict=app.cluster_dict)
+    
     with app.app_context():
-        app.session_status = True
-        app.summary_text = []
         if not Question.query.first():
             db.session.add(Question())
             db.session.commit()
+
+        app.session_status = True
+        app.cluster_dict = [[
+                    question.question_text for question in Question.query.all()
+                    ]]
+        app.summary_text = []
+
 
     return app
